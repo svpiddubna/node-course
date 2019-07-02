af04f4f9b6540ff274632074a61d3b765d6bbbe7
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-07-02 21:48:19 +0200

    first commit

---
+++ notes-app/app.js
@@ -0,0 +1,65 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+const command = process.argv[2];
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a note',
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: "Note description",
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.removeNote(argv.title);
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler() {
+    notes.listNotes();
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.readNote(argv.title);
+  }
+});
+
+yargs.parse();
