92c9ef37022b62320c44a9e7a1db6b8f0685f66a
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-07-07 14:54:53 +0200

    Set up for Heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -52,33 +53,15 @@ app.get('/weather', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log("Server is up on port 300");
-});
-
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
-
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
     })
 })
+
+
+app.listen(port, () => {
+  console.log("Server is up on port 300");
+});
