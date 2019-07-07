0b44fd2d3e490d01681394f055580dbf9e54da93
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-07-07 12:33:01 +0200

    Weather app add styles

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,24 +2,28 @@
 
 
 const search = document.querySelector('input')
-
 const weatherForm = document.querySelector('form')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+messageOne.textContent = ''
 
 weatherForm.addEventListener('submit', (event) => {
-
   event.preventDefault();
 
   const location = search.value
+  messageOne.textContent = 'Loading..'
+  messageTwo.textContent = ''
 
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
 
   response.json().then(data => {
 
     if (data.error) {
-      console.log(data.error)
+      messageOne.textContent = data.error
     } else {
-      console.log(data.location)
-      console.log(data.forecast)
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
     }
   })
 })
